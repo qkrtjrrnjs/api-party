26e366d983dc8a5647f8ba6ba9cb6ba6259eb386
Seok Kwun Park <park960@purdue.edu>
2018-06-18 10:02:32 -0400

    add header content & style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>api party</h1>
+        <div className="App-header">
+          <h1>api party</h1>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
